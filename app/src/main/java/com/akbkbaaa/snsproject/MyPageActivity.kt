22a6575a862f0f09d2a3495cfeb20a80cb687208
kim4152 <andykimjhjh@gmail.com>
2024-07-04 20:54:30 +0900

    김정호 - myPage

--- app/src/main/java/com/akbkbaaa/snsproject/MyPageActivity.kt
+++ app/src/main/java/com/akbkbaaa/snsproject/MyPageActivity.kt
@@ -1,11 +1,9 @@
 package com.akbkbaaa.snsproject
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -21,14 +19,16 @@ class MyPageActivity : AppCompatActivity() {
     private lateinit var myPageImage3: ImageView
     private lateinit var myPageImage4: ImageView
 
-    private lateinit var logout: TextView
+    private lateinit var logout: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_my_page)
 
-        val userId = Intent().getStringExtra("userId")!!
+
+
+        val userId = intent.getStringExtra("userId")!!
         init()
         setImage(userId)
         setProfile(userId)
